809237512d5fd2fb4f72a9544a1d51d79528465f
HP\daohu <daohuyhieu1704@gmail.com>
2024-07-11 21:52:15 +0700

    feat: make ui

---
+++ DHUI/Presenters/EngineViewport.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DHUI.Presenters
+{
+    /// <summary>
+    /// Interaction logic for EngineViewport.xaml
+    /// </summary>
+    public partial class EngineViewport : UserControl
+    {
+        public EngineViewport()
+        {
+            InitializeComponent();
+        }
+    }
+}
