30b32e6c9b247e6d833cc586292968eef70f9feb
HP\daohu <daohuyhieu1704@gmail.com>
2024-07-27 04:36:45 +0700

    ui properties

--- MathUI/Presenters/LeftSide.xaml.cs
+++ MathUI/Presenters/LeftSide.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathUI.ViewModels.LeftSide;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace MathUI.Presenters
         public LeftSide()
         {
             InitializeComponent();
+            DataContext = new LeftSideViewModel();
         }
     }
 }
